102edd8001962cbfd3b5c3960077f3a64f97b432
git-westriver <westriver.naoki20000316@gmail.com>
2025-04-13 15:58:08 +0900

    refactor: ph_trainer -> PHTrainer

--- ph_opt/ph_loss/algorithms.py
+++ ph_opt/ph_loss/algorithms.py
@@ -21,12 +21,16 @@ class PHOptimization:
         if (self.reg_obj is not None) and (not self.reg_proj):
             loss += self.reg_obj(self.X)
         return loss
+    
+    def update(self) -> None:
+        raise NotImplementedError
 
 class GradientDescent(PHOptimization):
     def __init__(self, X: torch.Tensor, loss_obj: PersistenceBasedLoss, reg_obj: Optional[Regularization]=None, reg_proj: bool=False, 
                  lr: float=1e-1, optimizer_conf: Optional[dict[str]]=None, scheduler_conf: Optional[dict[str]]=None):
         super().__init__(X, loss_obj, reg_obj, reg_proj)
-        ### optimizer ###
+
+        # optimizer
         if optimizer_conf is None:
             optimizer_conf = {"name": "SGD"}
         optimizer_name = optimizer_conf.get("name", "SGD")
@@ -34,7 +38,8 @@ class GradientDescent(PHOptimization):
             self.optimizer = torch.optim.SGD([self.X], lr=lr)
         elif optimizer_name == "Adam":
             self.optimizer = torch.optim.Adam([self.X], lr=lr)
-        ### scheduler ###
+
+        # scheduler
         if scheduler_conf is None:
             scheduler_conf = {"name": "const"}
         scheduler_name = scheduler_conf.get("name", "const")
@@ -60,7 +65,8 @@ class BigStep(PHOptimization):
     def __init__(self, X: torch.Tensor, loss_obj: PersistenceBasedLoss, reg_obj: Optional[Regularization]=None, reg_proj: bool=False, 
                  lr: float=1e-1, optimizer_conf: Optional[dict[str]]=None, scheduler_conf: Optional[dict[str]]=None):
         super().__init__(X, loss_obj, reg_obj, reg_proj)
-        ### optimizer ###
+
+        # optimizer
         if optimizer_conf is None:
             optimizer_conf = {"name": "SGD"}
         optimizer_name = optimizer_conf.get("name", "SGD")
@@ -68,7 +74,8 @@ class BigStep(PHOptimization):
             self.optimizer = torch.optim.SGD([self.X], lr=lr)
         elif optimizer_name == "Adam":
             self.optimizer = torch.optim.Adam([self.X], lr=lr)
-        ### scheduler ###
+
+        # scheduler
         if scheduler_conf is None:
             scheduler_conf = {"name": "const"}
         scheduler_name = scheduler_conf.get("name", "const")
@@ -79,14 +86,16 @@ class BigStep(PHOptimization):
 
     def update(self) -> None:
         self.optimizer.zero_grad()
-        ### compute the gradient of the persistence based loss at the current point ###
+
+        # compute the gradient of the persistence based loss at the current point
         loss = self.loss_obj(self.X)
         if loss.grad_fn is None:
             return
         loss.backward()
         gen_grad_norm = torch.norm(self.X.grad)
         self.optimizer.zero_grad()
-        ### get the target value of each simplex ###
+
+        # get the target value of each simplex
         rph = RipsPH(self.X, self.loss_obj.maxdim)
         rph.compute_ph(clearing_opt=False, get_inv=True)
         rph.compute_ph_right(get_inv=True)
@@ -123,7 +132,8 @@ class BigStep(PHOptimization):
                             high_target_value[dim+1][simp] = max(d_target, high_target_value[dim+1][simp])
                         else:
                             high_target_value[dim+1][simp] = d_target
-        ### Compute Auxilary Loss ###
+
+        # Compute Auxilary Loss
         aux_loss = 0
         for dim in range(self.loss_obj.maxdim+2):
             moving_simp = set(high_target_value[dim].keys()) | set(low_target_value[dim].keys())
@@ -141,22 +151,26 @@ class BigStep(PHOptimization):
                     aux_loss += torch.relu(high_target_value[dim][simp] - diam) ** 2
                 elif simp in low_target_value[dim]:
                     aux_loss += torch.relu(diam - low_target_value[dim][simp]) ** 2
-        ### Rescale Auxilary Loss to make the gradient norm of the auxilary loss equal to that of the persistence based loss ###
+
+        # Rescale Auxilary Loss to make the gradient norm of the auxilary loss equal to that of the persistence based loss
         if aux_loss.grad_fn is None:
             return
         aux_loss.backward(retain_graph=True)
         aux_grad_norm = torch.norm(self.X.grad)
         self.optimizer.zero_grad()
         aux_loss = aux_loss * gen_grad_norm / aux_grad_norm if aux_grad_norm > 0 else aux_loss
-        ### add the regularization, compute the gradient, and update the parameters ###
+
+        # add the regularization, compute the gradient, and update the parameters ###
         aux_loss = aux_loss + self.reg_obj(self.X)
         aux_loss.backward()
         self.optimizer.step()
         if self.scheduler is not None:
             self.scheduler.step()
-        ### we must delete rph to avoid memory leak ###
+
+        # delete rph to avoid memory leak
         del rph
-        ### project to the region of regularization = 0 ###
+
+        # project to the region of regularization = 0
         if self.reg_proj:
             self.X = self.reg_obj.projection(self.X)
 
@@ -164,7 +178,8 @@ class Diffeo(PHOptimization):
     def __init__(self, X: torch.Tensor, loss_obj: PersistenceBasedLoss, reg_obj: Optional[Regularization]=None, reg_proj: bool=False, 
                  lr: float=1e-1, sigma: float=0.1, optimizer_conf: Optional[dict[str]]=None, scheduler_conf: Optional[dict[str]]=None):
         super().__init__(X, loss_obj, reg_obj, reg_proj)
-        ### optimizer ###
+
+        # optimizer
         if optimizer_conf is None:
             optimizer_conf = {"name": "SGD"}
         optimizer_name = optimizer_conf.get("name", "SGD")
@@ -172,7 +187,8 @@ class Diffeo(PHOptimization):
             self.optimizer = torch.optim.SGD([self.X], lr=lr)
         elif optimizer_name == "Adam":
             self.optimizer = torch.optim.Adam([self.X], lr=lr)
-        ### scheduler ###
+
+        # scheduler
         if scheduler_conf is None:
             scheduler_conf = {"name": "const"}
         scheduler_name = scheduler_conf.get("name", "const")
@@ -180,19 +196,22 @@ class Diffeo(PHOptimization):
             self.scheduler = None
         elif scheduler_name == "TransformerLR":
             self.scheduler = TransformerLR(self.optimizer, warmup_epochs=scheduler_conf.get("warmup_epochs", 100))
-        ### unique parameter ###
+
+        # unique parameter
         self.sigma = sigma # the standard deviation of the Gaussian kernel
 
     def update(self) -> None:
         self.optimizer.zero_grad()
-        ### compute the gradient of the persistence based loss at the current point ###
+
+        # compute the gradient of the persistence based loss at the current point
         loss = self.loss_obj(self.X)
         if loss.grad_fn is None:
             return
         loss.backward()
         gen_grad = self.X.grad.clone()
         gen_grad_norm = torch.norm(self.X.grad)
-        ### compute the new gradient using diffeomorphic interpolation ###
+
+        # compute the new gradient using diffeomorphic interpolation
         nonzero_grad_idx = [idx for idx in range(self.num_pts) if gen_grad[idx, :].norm() > 0]
         nonzero_grad_X = self.X[nonzero_grad_idx, :]
         a_vec = torch.cat([gen_grad[i, :] for i in nonzero_grad_idx], dim=0) # corresponds to `a` in the paper
@@ -202,17 +221,20 @@ class Diffeo(PHOptimization):
         alpha = _alpha.view(len(nonzero_grad_idx), self.X.shape[1]) # corresponds to `alpha` in the paper
         rho_mat = torch.exp(- torch.cdist(self.X, nonzero_grad_X) ** 2 / (2 * self.sigma ** 2)) # shape = (num_pts, num_nonzero_grad_pts)
         new_grad = torch.einsum("ij,jk->ik", rho_mat, alpha).detach().clone() # shape = (num_pts, pts_dim)
-        ### obtain auxiliary loss based on the new gradient ###
+
+        # obtain auxiliary loss based on the new gradient
         self.optimizer.zero_grad()
         target_X = self.X.detach() -  new_grad
         aux_loss = (torch.norm(target_X - self.X) ** 2) * gen_grad_norm / (2 * torch.norm(new_grad))
-        ### add the regularization, compute the gradient, and update the parameters ###
+
+        # add the regularization, compute the gradient, and update the parameters
         aux_loss = aux_loss + self.reg_obj(self.X)
         aux_loss.backward()
         self.optimizer.step()
         if self.scheduler is not None:
             self.scheduler.step()
-        ### project to the region of regularization = 0 ###
+
+        # project to the region of regularization = 0
         if self.reg_proj:
             self.X = self.reg_obj.projection(self.X)
 
@@ -239,22 +261,28 @@ class Continuation(PHOptimization):
             raise ValueError("X.requires_grad must be True")
         if not bar_list: # nothing to do
             return X
-        ### compute \phi(x, y) = Pers(\Phi(x)) - y and its jacobian matrix ###
+        
+        # compute \phi(x, y) = Pers(\Phi(x)) - y and its jacobian matrix
         phi: list[torch.Tensor] = []
         _jacobi: list[torch.Tensor] = []
         for i in range(len(bar_list)):
             bv1, bv2, dv1, dv2 = bar_list[i].birth_v1, bar_list[i].birth_v2, bar_list[i].death_v1, bar_list[i].death_v2
             b_direction, d_direction = direction[i]
+
             # gradient of b_time - b_target w.t.t X = gradient of b_time w.t.t X (one element of the jacobian matrix)
             b_time: torch.Tensor = torch.norm(X[bv1] - X[bv2])
             _jacobi.append(torch.autograd.grad(b_time, X)[0])
+
             # gradient of d_time - d_target w.t.t X = gradient of d_time w.t.t X (one element of the jacobian matrix)
             d_time: torch.Tensor = torch.norm(X[dv1] - X[dv2])
             _jacobi.append(torch.autograd.grad(d_time, X)[0])
+
             # contain the value of Pers(\Phi(x)) - y in phi
             phi.append(-b_direction); phi.append(-d_direction) # NOTE: `phi` represents the value `current - target`
+
         phi_tensor: torch.Tensor = torch.stack(phi, dim=0).detach() # (2 * num_pairs)
         jacobi: torch.Tensor = torch.stack(_jacobi, dim=0) # (2 * num_pairs) x num_pts x pts_dim
+
         # update X using the pseudo-inverse of the jacobian matrix
         jacobi_flatten = jacobi.view(-1, jacobi.shape[1] * jacobi.shape[2]) # (2 * num_pairs) x (num_pts * pts_dim)
         jacobi_pinv = torch.linalg.pinv(jacobi_flatten).transpose(0, 1).view_as(jacobi) # (2 * num_pairs) x num_pts x pts_dim 
@@ -262,8 +290,9 @@ class Continuation(PHOptimization):
         return X
     
     def update(self) -> None:
-        ### get the direction to move for each bar ###
+        # get the direction to move for each bar
         direction_info = self.loss_obj.get_direction(self.X) # we can do this using giotto-ph
+
         # define bar_list and direction 
         bar_list: list[Bar] = []
         direction: list[torch.Tensor] = []
@@ -273,7 +302,8 @@ class Continuation(PHOptimization):
         if not bar_list: # nothing to do
             return
         direction = torch.stack(direction, dim=0)
-        ### inner loop ###
+
+        # inner loop
         for in_iter in range(self.in_iter_num):
             self.X = self.move_forward(self.X, bar_list, direction, self.lr)
             self.X = self.reg_obj.projection(self.X)
--- ph_opt/utils/ph_trainer.py
+++ ph_opt/utils/ph_trainer.py
@@ -15,7 +15,7 @@ import numpy as np
 from ph_opt import (
     PersistenceBasedLoss, ExpandLoss, 
     Regularization, RectangleRegularization,
-    GradientDescent, BigStep, Continuation, Diffeo, 
+    PHOptimization, GradientDescent, BigStep, Continuation, Diffeo, 
     get_animation
 )
 from ph_opt.data import circle_with_one_outlier, get_data
@@ -91,63 +91,115 @@ class PHTrainerConfig:
                 print(f"{k}: {v}")
         sys.stdout.flush()
 
-def ph_trainer(config: Optional[PHTrainerConfig] = None, scatter_config: Optional[dict] = None):
-    if config is None:
-        config = PHTrainerConfig()
-    config.print()
+class PHTrainer:
+    def __init__(self, config: Optional[PHTrainerConfig] = None, scatter_config: Optional[dict] = None):
+        if config is None:
+            config = PHTrainerConfig()
+        config.print()
 
-    ### Create a directory if it does not exist ###
-    save_dirpath = Path(config.save_dirpath) / config.exp_name if config.exp_name != "" else Path(config.save_dirpath)
-    if not os.path.exists(save_dirpath):
-        os.makedirs(save_dirpath)
+        self.config = config
+        self.scatter_config = scatter_config
 
-    ### Read data ###
-    if callable(config.data_source):
-        dataset = get_data(config.data_source, 100)
-    elif isinstance(config.data_source, str) or isinstance(config.data_source, Path):
-        data = np.load(config.data_source)
-        dataset = [data for _ in range(config.num_trial)]
-    else:
-        raise ValueError("data_source must be a function, str, or Path.")
+    def train(self):
+        # Create a directory if it does not exist
+        self._maybe_create_dir()
 
-    ### Optimization for `num_trial` different initial values ###
-    loss_history_list: list[list[float]] = []
-    time_history_list: list[list[float]] = []
-    for trial in range(config.num_trial):
-        print(f"--- Trial {trial} ---")
-        trial_start = time.time()
+        # Read data
+        dataset = self.read_data()
 
-        ## Initialize the variables ##
-        if config.init_strategy is not None:
-            X = config.init_strategy(dataset[trial])
+        # Optimization for `num_trial` different initial values
+        loss_history_list: list[list[float]] = []
+        time_history_list: list[list[float]] = []
+        for trial in range(self.config.num_trial):
+            print(f"--- Trial {trial} ---")
+            trial_start = time.time()
+
+            # Initialize the variable
+            X = self.initialize_variable(dataset, trial)
+
+            # Initialize the optimization method
+            poh = self.initialize_method(X)
+            
+            # Optimization
+            X_history, loss_history, time_history = self.run_optimization(poh)
+
+            # add loss_history, time_history to the list
+            loss_history_list.append(loss_history)
+            time_history_list.append(time_history)
+
+            # For the first trial, save X_history and create a gif
+            if trial == 0:
+                # save X_history
+                with open(self.config.save_dirpath / "X_history.pkl", "wb") as f:
+                    pickle.dump(X_history, f)
+
+                # create a gif
+                anim = get_animation([X_history], [[loss_history]], 
+                                    dim_list=self.config.loss_obj.dim_list,
+                                    title_list=[self.config.method], 
+                                    vertical=False, 
+                                    scatter_config=self.scatter_config)
+                anim.save(self.config.save_dirpath / "trajectory.gif", writer='pillow')
+
+            # Finish the trial
+            print(f"Trial {trial} finished. elapsed time: {time.time() - trial_start}", flush=True)
+
+        # save loss_history_list and time_history_list
+        result_dict = {
+            "loss_history": loss_history_list,
+            "time_history": time_history_list,
+        }
+        with open(self.config.save_dirpath / "result_dict.pkl", "wb") as f:
+            pickle.dump(result_dict, f)
+    
+    def _maybe_create_dir(self):
+        save_dirpath = Path(self.config.save_dirpath) / self.config.exp_name if self.config.exp_name != "" else Path(self.config.save_dirpath)
+        if not os.path.exists(save_dirpath):
+            os.makedirs(save_dirpath)
+
+    def read_data(self):
+        if callable(self.config.data_source):
+            dataset = get_data(self.config.data_source, 100)
+        elif isinstance(self.config.data_source, str) or isinstance(self.config.data_source, Path):
+            data = np.load(self.config.data_source)
+            dataset = [data for _ in range(self.config.num_trial)]
+        else:
+            raise ValueError("data_source must be a function, str, or Path.")
+        return dataset
+    
+    def initialize_variable(self, dataset, trial):
+        if self.config.init_strategy is not None:
+            X = self.config.init_strategy(dataset[trial])
         else:
             X = torch.from_numpy(dataset[trial])
         X = X.float().requires_grad_()
-
-        ## Initialize the optimization method ##
-        if config.method == "gd":
-            poh = GradientDescent(X, config.loss_obj, config.regularization_obj, reg_proj=config.reg_proj, 
-                                  lr=config.lr, optimizer_conf=config.optimizer_conf, scheduler_conf=config.scheduler_conf)
-        elif config.method == "bigstep":
-            poh = BigStep(X, config.loss_obj, config.regularization_obj, reg_proj=config.reg_proj, 
-                          lr=config.lr, optimizer_conf=config.optimizer_conf, scheduler_conf=config.scheduler_conf)
-        elif config.method == "diffeo":
-            poh = Diffeo(X, config.loss_obj, config.regularization_obj, reg_proj=config.reg_proj, 
-                        lr=config.lr, optimizer_conf=config.optimizer_conf, scheduler_conf=config.scheduler_conf)
-        elif config.method == "continuation":
-            poh = Continuation(X, config.loss_obj, config.regularization_obj, 
-                               lr=config.lr, in_iter_num=config.num_in_iter)
+        return X
+    
+    def initialize_method(self, X) -> PHOptimization:
+        if self.config.method == "gd":
+            poh = GradientDescent(X, self.config.loss_obj, self.config.regularization_obj, reg_proj=self.config.reg_proj, 
+                                  lr=self.config.lr, optimizer_conf=self.config.optimizer_conf, scheduler_conf=self.config.scheduler_conf)
+        elif self.config.method == "bigstep":
+            poh = BigStep(X, self.config.loss_obj, self.config.regularization_obj, reg_proj=self.config.reg_proj, 
+                          lr=self.config.lr, optimizer_conf=self.config.optimizer_conf, scheduler_conf=self.config.scheduler_conf)
+        elif self.config.method == "diffeo":
+            poh = Diffeo(X, self.config.loss_obj, self.config.regularization_obj, reg_proj=self.config.reg_proj, 
+                        lr=self.config.lr, optimizer_conf=self.config.optimizer_conf, scheduler_conf=self.config.scheduler_conf)
+        elif self.config.method == "continuation":
+            poh = Continuation(X, self.config.loss_obj, self.config.regularization_obj, 
+                               lr=self.config.lr, in_iter_num=self.config.num_in_iter)
         else:
             raise NotImplementedError
-        
-        ## Optimization ##
+        return poh
+    
+    def run_optimization(self, poh: PHOptimization) -> tuple[list[torch.Tensor], list[float], list[float]]:
         X_history = []
         loss_history = []
         time_history = [0]
         epoch = -1
         elapsed_time = 0
-        while ((config.num_epoch is not None and epoch < config.num_epoch - 1) 
-                or (config.time_limit is not None and elapsed_time < config.time_limit)):
+        while ((self.config.num_epoch is not None and epoch < self.config.num_epoch - 1)
+                or (self.config.time_limit is not None and elapsed_time < self.config.time_limit)):
             epoch += 1
             elapsed_time += time_history[-1]
 
@@ -155,7 +207,7 @@ def ph_trainer(config: Optional[PHTrainerConfig] = None, scatter_config: Optiona
             loss = poh.get_loss()
             loss_history.append(loss.item())
             X_history.append(poh.X.detach().clone())
-            if epoch % config.log_interval == 0:
+            if epoch % self.config.log_interval == 0:
                 print(f"epoch: {epoch}, loss: {loss.item()}", flush=True)
 
             # update the variables with the specified method and measure the time of epoch
@@ -168,93 +220,64 @@ def ph_trainer(config: Optional[PHTrainerConfig] = None, scatter_config: Optiona
         X_history.append(poh.X.detach().clone())
         print(f"Final loss: {loss.item()}", flush=True)
 
-        ## Finish the optimization and record the results ##
         # convert time_history to cumulative time
         time_history = list(accumulate(time_history))
 
-        # add loss_history, time_history to the list
-        loss_history_list.append(loss_history)
-        time_history_list.append(time_history)
-
-        ## For the first trial, save X_history and create a gif ##
-        if trial == 0:
-            # save X_history
-            with open(save_dirpath / "X_history.pkl", "wb") as f:
-                pickle.dump(X_history, f)
-
-            # create a gif
-            anim = get_animation([X_history], [[loss_history]], 
-                                dim_list=config.loss_obj.dim_list,
-                                title_list=[config.method], 
-                                vertical=False, 
-                                scatter_config=scatter_config)
-            anim.save(save_dirpath / "trajectory.gif", writer='pillow')
-
-        ## Finish the trial ##
-        print(f"Trial {trial} finished. elapsed time: {time.time() - trial_start}", flush=True)
-
-    ### Visualization and saving the results ###
-    ## visualization of the transition of the loss over epochs ##
-    if config.num_epoch is None: 
-        # Then, scatter between epoch and loss
-        fig = plt.figure(dpi=300)
-        ax = fig.add_subplot(111)
-        for trial in range(config.num_trial):
-            ax.scatter(np.arange(len(loss_history_list[trial])), loss_history_list[trial], color="blue", alpha=0.3)
-        ax.set_xlabel("epoch")
-        ax.set_ylabel("loss")
-        fig.savefig(save_dirpath / "epoch-loss.png")
-    else: 
-        # Then, mean and std of the loss over trials
-        loss_history_mat = np.stack(loss_history_list, axis=0) # (num_trial, num_epoch+1)
-        loss_mean = np.mean(loss_history_mat, axis=0)
-        loss_std = np.std(loss_history_mat, axis=0)
-        fig = plt.figure(dpi=300)
-        ax = fig.add_subplot(111)
-        ax.plot(loss_mean, color="blue")
-        ax.fill_between(np.arange(loss_mean.shape[0]), loss_mean - loss_std, loss_mean + loss_std, color="blue", alpha=0.3)
-        ax.set_xlabel("epoch")
-        ax.set_ylabel("loss")
-        fig.savefig(save_dirpath /"epoch-loss.png")
-
-    ## visualization of the transition of the loss over time ##
-    if config.time_limit is None: 
-        # Then, scatter between time and loss
-        fig = plt.figure(dpi=300)
-        ax = fig.add_subplot(111)
-        for trial in range(config.num_trial):
-            ax.scatter(time_history_list[trial], loss_history_list[trial], color="blue", alpha=0.3)
-        ax.set_xlabel("time")
-        ax.set_ylabel("loss")
-        fig.savefig(f"{save_dirpath}/time-loss.png")
-    else: 
-        # Then, mean and std of the loss over trials
-        time_linspace = np.linspace(0, config.time_limit, 101)
-        time_loss_list = [[loss_history_list[trial][0]] for trial in range(config.num_trial)]
-        for trial in range(config.num_trial):
-            # loss value at time t is the last loss value before t
-            # Note: loss_history_list[trial][cur_epoch] is after cur_epoch, loss_history_list[trial][cur_epoch-1] is before cur_epoch
-            cur_epoch = 0 
-            for t in time_linspace[1:]:
-                while cur_epoch < len(loss_history_list[trial]) - 1 and time_history_list[trial][cur_epoch] < t:
-                    cur_epoch += 1
-                time_loss_list[trial].append(loss_history_list[trial][cur_epoch])
-        time_loss_mat = np.stack(time_loss_list, axis=0) # (num_trial + 1, 101)
-        time_loss_mean = np.mean(time_loss_mat, axis=0)
-        time_loss_std = np.std(time_loss_mat, axis=0)
-        fig = plt.figure(dpi=300)
-        ax = fig.add_subplot(111)
-        ax.plot(time_linspace, time_loss_mean, color="blue")
-        ax.fill_between(time_linspace, time_loss_mean - time_loss_std, time_loss_mean + time_loss_std, color="blue", alpha=0.3)
-        ax.set_xlabel("time")
-        ax.set_ylabel("loss")
-        fig.savefig(save_dirpath / "time-loss.png")
-
-    ## save loss_history_list and time_history_list ##
-    result_dict = {
-        "loss_history": loss_history_list,
-        "time_history": time_history_list,
-    }
-    with open(save_dirpath / "result_dict.pkl", "wb") as f:
-        pickle.dump(result_dict, f)
+        return X_history, loss_history, time_history
     
+    def visualize_loss(self, loss_history_list: list[list[float]], time_history_list: list[list[float]]):
+        ## visualization of the transition of the loss over epochs ##
+        if self.config.num_epoch is None: 
+            # Then, scatter between epoch and loss
+            fig = plt.figure(dpi=300)
+            ax = fig.add_subplot(111)
+            for trial in range(self.config.num_trial):
+                ax.scatter(np.arange(len(loss_history_list[trial])), loss_history_list[trial], color="blue", alpha=0.3)
+            ax.set_xlabel("epoch")
+            ax.set_ylabel("loss")
+            fig.savefig(self.config.save_dirpath / "epoch-loss.png")
+        else:
+            # Then, mean and std of the loss over trials
+            loss_history_mat = np.stack(loss_history_list, axis=0) # (num_trial, num_epoch+1)
+            loss_mean = np.mean(loss_history_mat, axis=0)
+            loss_std = np.std(loss_history_mat, axis=0)
+            fig = plt.figure(dpi=300)
+            ax = fig.add_subplot(111)
+            ax.plot(loss_mean, color="blue")
+            ax.fill_between(np.arange(loss_mean.shape[0]), loss_mean - loss_std, loss_mean + loss_std, color="blue", alpha=0.3)
+            ax.set_xlabel("epoch")
+            ax.set_ylabel("loss")
+            fig.savefig(self.config.save_dirpath /"epoch-loss.png")
+
+        ## visualization of the transition of the loss over time ##
+        if self.config.time_limit is None: 
+            # Then, scatter between time and loss
+            fig = plt.figure(dpi=300)
+            ax = fig.add_subplot(111)
+            for trial in range(self.config.num_trial):
+                ax.scatter(time_history_list[trial], loss_history_list[trial], color="blue", alpha=0.3)
+            ax.set_xlabel("time")
+            ax.set_ylabel("loss")
+            fig.savefig(f"{self.config.save_dirpath}/time-loss.png")
+        else: 
+            # Then, mean and std of the loss over trials
+            time_linspace = np.linspace(0, self.config.time_limit, 101)
+            time_loss_list = [[loss_history_list[trial][0]] for trial in range(self.config.num_trial)]
+            for trial in range(self.config.num_trial):
+                # loss value at time t is the last loss value before t
+                # Note: loss_history_list[trial][cur_epoch] is after cur_epoch, loss_history_list[trial][cur_epoch-1] is before cur_epoch
+                cur_epoch = 0 
+                for t in time_linspace[1:]:
+                    while cur_epoch < len(loss_history_list[trial]) - 1 and time_history_list[trial][cur_epoch] < t:
+                        cur_epoch += 1
+                    time_loss_list[trial].append(loss_history_list[trial][cur_epoch])
+            time_loss_mat = np.stack(time_loss_list, axis=0) # (num_trial + 1, 101)
+            time_loss_mean = np.mean(time_loss_mat, axis=0)
+            time_loss_std = np.std(time_loss_mat, axis=0)
+            fig = plt.figure(dpi=300)
+            ax = fig.add_subplot(111)
+            ax.plot(time_linspace, time_loss_mean, color="blue")
+            ax.fill_between(time_linspace, time_loss_mean - time_loss_std, time_loss_mean + time_loss_std, color="blue", alpha=0.3)
+            ax.set_xlabel("time")
+            ax.set_ylabel("loss")
+            fig.savefig(self.config.save_dirpath / "time-loss.png")
